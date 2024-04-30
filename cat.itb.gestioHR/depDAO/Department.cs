2ae187a77eab068b596a397a2134a35a5aecee8b
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-04-30 10:47:25 +0200

    Implemented selectAll and insert all on fileDepartment

--- cat.itb.gestioHR/depDAO/Department.cs
+++ cat.itb.gestioHR/depDAO/Department.cs
@@ -5,7 +5,11 @@
         public virtual int _id { get; set; }
         public virtual string Name { get; set; }
         public virtual string Loc { get; set; }
-        
+
+        public override string ToString()
+        {
+            return $"{{{nameof(_id)}={_id.ToString()}, {nameof(Name)}={Name}, {nameof(Loc)}={Loc}}}";
+        }
     }
 }
 
