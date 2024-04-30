652aa9a5e4d94c180128ff8c9d35b35847dfd910
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-04-30 09:45:23 +0200

    Add project files.

---
+++ cat.itb.gestioHR/connections/SQLConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Npgsql;
+
+namespace cat.itb.gestioHR.connections
+{
+    public class SQLConnection
+    {
+        private String HOST = "balarama.db.elephantsql.com:5432"; // Ubicació de la BD.
+        private String DB = "qylrvsaa"; // Nom de la BD.
+        private String USER = "qylrvsaa";
+        private String PASSWORD = "WApqM0DJGoMManfagt-fBh-8r8wrRUyI";
+
+        public NpgsqlConnection GetConnection()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(
+                "Host=" + HOST + ";" + "Username=" + USER + ";" +
+                "Password=" + PASSWORD + ";" + "Database=" + DB + ";"
+            );
+            conn.Open();
+            return conn;
+        }
+    }
+}
